6eca5efc1c1c5c3378eb3118cbb28926a9a183d8
MohammedaijazS-Hexaware <aijazsayed757@gmail.com>
2020-06-13 19:22:03 +0530

    another

--- routers/REST.js
+++ routers/REST.js
@@ -20,15 +20,12 @@ router.get('/:id',async(req,res)=>{
     try{
         const List= await Entire.findById(req.params.id)
         if(List==null){
-            console.log('In If')
             res.status(200).json({Message:'No Particular ToDo List'})
         }
         else{
-            console.log('Here')
             res.status(200).json(List)
         }
     }catch(err){
-        console.log('In Catch')
         res.status(200).send('Error'+err)
     }
 })
@@ -51,9 +48,14 @@ router.post('/',async(req,res) => {
 router.patch('/:id',async(req,res) => {
     try{
         const tD =await Entire.findById(req.params.id)
-        tD.ToDo=req.body.ToDo
-        const tD1=await tD.save()
-        res.json(tD1)
+        if(tD==null){
+            res.status(200).json({Message:'No Particular ToDo List'})
+        }
+        else{
+            tD.ToDo=req.body.ToDo
+            const tD1=await tD.save()
+            res.status(200).json(tD1)
+        }
     }catch(err){
         res.send('Error'+err)
     }
@@ -62,8 +64,13 @@ router.patch('/:id',async(req,res) => {
 router.delete('/:id',async(req,res) => {
     try{
         const tD =await Entire.findById(req.params.id)
-        const tD1=await tD.remove()
-        res.send('Deleted')
+        if(tD==null){
+            res.status(200).json({Message:'No Particular ToDo List'})
+        }
+        else{
+            const tD1=await tD.remove()
+            res.send('Deleted')
+        }
     }catch(err){
         res.send('Error'+err)
     }
